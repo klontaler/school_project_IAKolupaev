55e760b99ab16b01cb03b624deebf2a44d2e2f54
klontaler <fcknguglyandsotired@gmail.com>
2022-12-29 09:18:40 +1000

    header_transform mobile fix

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ let header = document.getElementById('header');
 
 window.addEventListener('scroll', function(){
   scrollTop = pageYOffset;
-  if (scrollTop > lastScrollTop) {
-    header.classList.add('header_transform');
-  } else {
-    header.classList.remove('header_transform')
+  if (scrollTop > 96) {
+    if (scrollTop > lastScrollTop) {
+      header.classList.add('header_transform');
+    } else {
+      header.classList.remove('header_transform')
+    }  
   }
   lastScrollTop = scrollTop;
 });
